81f9f5bce80762e4b085a48db659920a93e8853f
Vadem <101812203+Vadem1689@users.noreply.github.com>
2023-08-02 12:11:26 +0300

    Ready

---
+++ Assets/Scripts/Enemy/StateMachin/Transition/TargetDiedTransition.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDiedTransition : Transition
+{
+    private void Update()
+    {
+        if (Target==null)
+        {
+            NeedTransition = true;
+        }
+    }
+}
